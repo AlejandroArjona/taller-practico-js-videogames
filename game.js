e7fd0501d5ef8d029070369ffae46218ab6b838e
AlejandroArjona <a96.godoy@hotmail.com>
2023-08-23 23:28:31 -0600

    Eventos y botones

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnDown = document.querySelector('#down');
 
 let canvasSize;
 let elementsSize;
@@ -22,23 +26,19 @@ function setCanvasSize () {
 }
 
 function startGame () {
-    console.log({canvasSize, elementsSize});
-
     game.font = elementsSize + 'px Verdana';
     game.textAlign = 'end';
 
     const map = maps[0];
     const mapRows = map.trim().split('\n');
     const mapRowCols = mapRows.map(row => row.trim().split(''));
-    console.log({map, mapRows, mapRowCols});
 
     mapRowCols.forEach((row, rowI) => {
         row.forEach((col, colI) => {
             const emoji = emojis[col];
-            const posX = elementsSize * (colI + 1);
-            const posY = elementsSize * (rowI + 1);
+            const posX = elementsSize * (colI + 1) + 10;
+            const posY = elementsSize * (rowI + 1) - 8;
             game.fillText(emoji, posX, posY);
-            console.log({row, rowI, col, colI});
         })
     })
 
@@ -47,4 +47,37 @@ function startGame () {
             game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col + 10, elementsSize * row - 8);
         }
     } */
+}
+
+window.addEventListener('keydown', moveByKeys);
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
+
+/* function moveByKeys (event) {
+    console.log(event);
+} */
+function moveByKeys (event) {
+    if (event.key == 'ArrowUp') {
+        moveUp();
+    }else if (event.key == 'ArrowLeft'){
+        moveLeft();
+    }else if (event.key == 'ArrowRight'){
+        moveRight();
+    }else if (event.key == 'ArrowDown'){
+        moveDown();
+    }
+}
+function moveUp () {
+    console.log('Me quiero mover hacia arriba');
+}
+function moveLeft () {
+    console.log('Me quiero mover hacia izquierda');
+}
+function moveRight () {
+    console.log('Me quiero mover hacia derecha');
+}
+function moveDown () {
+    console.log('Me quiero mover hacia abajo');
 }
